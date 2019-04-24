c2c351c0d66ae7ff325fa61133c32783aa34b063
prajurock <prajwalsingh651@gmail.com>
2019-04-24 20:07:39 +0530

    Add Base Activity

---
+++ app/src/main/java/com/example/mvp/BaseAcivity.java
@@ -0,0 +1,45 @@
+package com.example.mvp;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public abstract class BaseAcivity extends AppCompatActivity {
+
+
+  private ProgressDialog mProgressDialog;
+
+  @Override
+  protected void onCreate(@Nullable Bundle savedInstanceState){
+    super.onCreate(savedInstanceState);
+    setContentView(getContentView());
+    onViewReady(savedInstanceState,getIntent());
+  }
+
+
+  protected void onViewReady(Bundle savedInstanceState, Intent intent) {
+    //To be used by child Activity
+    
+  }
+
+  protected void showDialog(String message){
+    if(mProgressDialog==null){
+      mProgressDialog=new ProgressDialog(this);
+      mProgressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+      mProgressDialog.setCancelable(true);
+    }
+    mProgressDialog.setMessage(message);
+    mProgressDialog.show();
+  }
+
+  protected void hideDialog(){
+    if(mProgressDialog!=null && mProgressDialog.isShowing()){
+      mProgressDialog.dismiss();
+    }
+  }
+
+
+  protected abstract int getContentView();
+}
